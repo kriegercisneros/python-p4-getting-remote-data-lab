5cd82f2f5031fcd2ee7fbb9c8a45447ab7d86e49
JackieCisneros <krieger.jacqueline@gmail.com>
2023-04-04 12:58:29 -0600

    finished with help from solutions

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        #sends a GET request to the URL passed in on initialization. This method should return the body 
+        #of the response
+        #why do i use self.url insead of url? 
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        #use get_response_body to send a request, then return a Python list or dictionary made up of 
+        #data converted from the response of that request
+        #why do i need to call self on the get_response_body??  
+        return json.loads(self.get_response_body())
